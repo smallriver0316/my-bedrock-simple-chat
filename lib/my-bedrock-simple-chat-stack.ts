3063807fb84514cef00220c9fd1fd897352b6918
smallriver0316 <stream0316@gmail.com>
2024-09-08 06:12:01 +0900

    Added VPC to infrastructure

--- lib/my-bedrock-simple-chat-stack.ts
+++ lib/my-bedrock-simple-chat-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -9,12 +11,56 @@ export class MyBedrockSimpleChatStack extends cdk.Stack {
 
     const stage = this.node.tryGetContext('stage') || 'dev';
 
+    // DynamoDB
     const table = new dynamodb.TableV2(this, `Table-${stage}`, {
       tableName: `MyBedrockSimpleChat_Table_${stage}`,
       partitionKey: { name: 'UserId', type: dynamodb.AttributeType.STRING },
       sortKey: { name: 'SessionId', type: dynamodb.AttributeType.STRING },
       billing: dynamodb.Billing.onDemand(),
       removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // VPC
+    const vpc = new ec2.Vpc(this, `Vpc-${stage}`, {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'public-subnet',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'private-subnet',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    });
+
+    vpc.addGatewayEndpoint(`DynamoDbEndpoint-${stage}`, {
+      service: ec2.GatewayVpcEndpointAwsService.DYNAMODB,
+      subnets: [{ subnetType: ec2.SubnetType.PRIVATE_ISOLATED }],
+    });
+
+    vpc.addInterfaceEndpoint(`EcrEndpoint-${stage}`, {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR,
+      subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    });
+
+    vpc.addInterfaceEndpoint(`EcrDockerEndpoint-${stage}`, {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
+      subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    });
+
+    vpc.addInterfaceEndpoint(`CloudwatchEndpoint-${stage}`, {
+      service: ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
+      subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    });
+
+    vpc.addInterfaceEndpoint(`BedrockEndpoint-${stage}`, {
+      service: ec2.InterfaceVpcEndpointAwsService.BEDROCK_RUNTIME,
+      subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
     });
     // example resource
     // const queue = new sqs.Queue(this, 'MyBedrockSimpleChatQueue', {
